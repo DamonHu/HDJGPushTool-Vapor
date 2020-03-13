391483f6921db42e6e36f8c6e2036d761098f4b3
Damon <dong765@qq.com>
2020-03-13 15:18:00 +0800

    U 修改响应值
    U 修改响应值

--- Sources/HDJGPushTool/HDJGPushTool.swift
+++ Sources/HDJGPushTool/HDJGPushTool.swift
@@ -18,17 +18,15 @@ class HDJGPushTool {
     }
     //推送通知
     @discardableResult
-    func pushNotification(_ req: Request, pushModel: HDJGPushModel) throws -> Future<Res_NoDataModel> {
+    func pushNotification(_ req: Request, pushModel: HDJGPushModel) throws -> Future<HTTPResponse> {
         let maskSecret = "\(self.appKey):\(self.appSecrect)"
         let base64Data = maskSecret.data(using: String.Encoding.utf8)
         let base64String: String = base64Data?.base64EncodedString() ?? ""
         
         let client = try req.make(Client.self)
         let request = Request(http: HTTPRequest(method: HTTPMethod.POST, url: URL(string: "https://api.jpush.cn/v3/push")!, headers: HTTPHeaders([("Authorization" , "Basic " + base64String)]), body: try pushModel.getRequestJsonData()), using: req)
-        return client.send(request).map { (response) -> Res_NoDataModel in
-            print(response)
-            let res_NoDataModel = Res_NoDataModel()
-            return res_NoDataModel
+        return client.send(request).map { (response) -> HTTPResponse in
+            return response.http
         }
     }
 }
