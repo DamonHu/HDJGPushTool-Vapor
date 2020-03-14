69c7f4b0a2bb344f9c696ac044ba4a296ac75756
Damon <dong765@qq.com>
2020-03-14 17:06:25 +0800

    更新依赖
    更新依赖

--- Package.swift
+++ Package.swift
@@ -17,14 +17,18 @@ let package = Package(
 //        .package(url: "https://github.com/vapor/crypto.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+
+        // Bindings to OpenSSL-compatible libraries for TLS support in SwiftNIO
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HDJGPushTool",
-            dependencies: ["Core", "Crypto"]),
+            dependencies: ["Core", "Crypto", "NIO", "NIOOpenSSL"]),
         .testTarget(
             name: "HDJGPushToolTests",
             dependencies: ["HDJGPushTool"]),
