22413118bfc6d5691e14cd34ca39ff31abbb09f1
Damon <dong765@qq.com>
2020-03-14 16:57:46 +0800

    更新依赖
    更新依赖

--- Package.swift
+++ Package.swift
@@ -15,14 +15,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(url: "https://github.com/vapor/crypto.git", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
+        // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HDJGPushTool",
-            dependencies: ["Vapor"]),
+            dependencies: ["Core", "Crypto"]),
         .testTarget(
             name: "HDJGPushToolTests",
             dependencies: ["HDJGPushTool"]),
