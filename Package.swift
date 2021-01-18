7c0a307a183b3308881f12403ee40e5c6be0bab3
Damon <dong765@qq.com>
2021-01-18 10:33:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "HDJGPushTool",
-            dependencies: ["Vapor"]),
-        .testTarget(
-            name: "HDJGPushToolTests",
-            dependencies: ["HDJGPushTool"]),
+        .target(name: "HDJGPushTool", dependencies: [
+            .product(name: "Vapor", package: "vapor")
+        ]),
+        .testTarget(name: "HDJGPushToolTests", dependencies: [
+            .target(name: "App")
+        ]),
     ]
 )
