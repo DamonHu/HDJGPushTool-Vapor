cca23553621f68765315d4f554ad7169a44013bc
Damon <dong765@qq.com>
2021-01-19 17:29:47 +0800

    update: 4.0
    update: 4.0

--- Sources/HDJGPushTool/HDJGPushTool.swift
+++ Sources/HDJGPushTool/HDJGPushTool.swift
@@ -6,28 +6,28 @@
 //
 
 import Vapor
-import Crypto
 
 public class HDJGPushTool {
     private var appKey = ""
     private var appSecrect = ""
-    
+
     public required init(appKey: String, appSecrect: String) {
         self.appKey = appKey
         self.appSecrect = appSecrect
     }
     //推送通知
     @discardableResult
-    public func pushNotification(_ req: Request, pushModel: HDJGPushModel) throws -> EventLoopFuture<HTTPResponse> {
+    public func pushNotification(_ req: Request, pushModel: HDJGPushModel) throws -> EventLoopFuture<HTTPResponseStatus> {
         let maskSecret = "\(self.appKey):\(self.appSecrect)"
         let base64Data = maskSecret.data(using: String.Encoding.utf8)
         let base64String: String = base64Data?.base64EncodedString() ?? ""
-        
-        let client = try req.make(Client.self)
-        let request = Request(http: HTTPRequest(method: HTTPMethod.POST, url: URL(string: "https://api.jpush.cn/v3/push")!, headers: HTTPHeaders([("Authorization" , "Basic " + base64String)]), body: try pushModel.getRequestJsonData()), using: req)
-        return client.send(request).map { (response) -> HTTPResponse in
-            print(response)
-            return response.http
+
+        return req.client.post("https://api.jpush.cn/v3/push", headers: HTTPHeaders([("Authorization" , "Basic " + base64String)])) { (req) in
+            if let jsonData =  try? pushModel.getRequestJsonData() {
+                try req.content.encode(String.init(data: jsonData, encoding: .utf8) ?? "")
+            }
+        }.map { (res) -> (HTTPResponseStatus) in
+            return .ok
         }
     }
 }
